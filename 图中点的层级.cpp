92ad8fb7d0fc7b29b9fd80f0d72dd6f0e9af104e
lixiaoyusgithub <89776576+lixiaoyusgithub@users.noreply.github.com>
2023-04-23 11:14:31 +0800

    更新把数码

---
+++ 图中点的层级.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+const int N = 100010;
+int h[N], e[N], ne[N], idx;
+int q[N], d[N];
+int n, m;
+void add(int a, int b)
+{
+	e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+int bfs()
+{
+	int hh = 0, tt = 0;
+	q[0] = 1;
+	memset(d, -1, sizeof d);
+	d[1] = 0;
+	while (hh <= tt)
+	{
+		int t = q[hh++];
+		for (int i = h[t]; i != -1; i = ne[i])
+		{
+			int j = e[i];
+			if (d[j] != -1)
+			{
+				q[++tt] = j;
+				d[j] = d[t] + 1;
+			}
+		}
+	}
+	return d[n];
+}
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		add(a, b);
+	}
+	cout << bfs() << endl;
+	return 0;
+}
